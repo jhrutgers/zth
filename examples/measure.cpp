b3d9e6096998fac0b7fd6d1b3d3c1f245fe93b12
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2019-09-30 23:28:42 +0200

    measure test

--- examples/measure.cpp
+++ examples/measure.cpp
@@ -1,141 +1,207 @@
 #include <zth>
 
-#define MEASURE_TARGET_TIME_s 1.0
+/////////////////////////////////////////////////
+// Tests
 
-class FsmGuard {
-public:
-	template <typename Fsm>
-	FsmGuard(bool (*f)(Fsm&)) : m_f(reinterpret_cast<void*>(f)) { printf("guard %p = %p\n", this, m_f); }
+void testEmpty() {
+}
 
-	FsmGuard(void*) : m_f() {}
+void testNow() {
+	zth::Timestamp::now();
+}
 
-	template <typename Fsm>
-	bool operator()(Fsm& fsm) { return reinterpret_cast<bool(Fsm&)>(m_f)(fsm); }
+static zth::Context* context1;
+static zth::Context* context2;
 
-	void* f() const { return m_f; }
+void context2entry(void*) {
+	while(true) {
+		zth::context_switch(context2, context1);
+	}
+}
 
-private:
-	void* m_f;
+void testContextInit() {
+	int res;
+	context1 = zth::currentFiber().context();
+	if((res = zth::context_create(context2, zth::ContextAttr(&context2entry))))
+		zth::abort("Cannot create context2; %s", zth::err(res).c_str());
+}
+
+void testContext() {
+	zth::context_switch(context1, context2);
+}
+
+void testContextCleanup() {
+	zth::context_destroy(context2);
+}
+
+void testYield1() {
+	zth::yield();
+}
+
+void testYield1Always() {
+	zth::yield(NULL, true);
+}
+
+static zth::Fiber* testYieldFiberPtr = NULL;
+void testYieldFiber() {
+	testYieldFiberPtr = &zth::currentFiber();
+
+	while(true)
+		zth::yield(NULL, true);
+}
+zth_fiber(testYieldFiber)
+
+void testYieldInit() {
+	async testYieldFiber();
+}
+
+void testYield() {
+	zth::yield(NULL, true);
+}
+
+void testYieldCleanup() {
+	if(!testYieldFiberPtr)
+		zth::abort("Test fiber did not run");
+
+	testYieldFiberPtr->kill();
+}
+
+void testFiberCreateEntry()
+{
+}
+zth_fiber(testFiberCreateEntry)
+
+void testFiberCreate() {
+	testFiberCreateEntry_future f = async testFiberCreateEntry();
+	f->wait();
+}
+
+/////////////////////////////////////////////////
+// Tester
+
+typedef enum { stateInit, stateCalibrate, stateMeasure, stateDone } State;
+namespace zth { template <> inline cow_string str<State>(State value) { return str((int)value); } }
+
+struct TestData {
+	char const* description;
+	void (*test)();
+	double singleResult;
+	size_t calibration;
+	zth::Timestamp start;
+	zth::TimeInterval duration;
 };
 
-bool always(int& fsm) { return true; }
+typedef zth::FsmCallback<State,TestData*> Fsm_type;
 
-struct FsmDescriptionPair {
-	int state;
-	FsmGuard guard;
+static zth::TimeInterval maxIterations(Fsm_type& fsm) {
+	return fsm.callbackArg()->calibration > std::numeric_limits<size_t>::max() / 2
+		? zth::TimeInterval() : zth::TimeInterval(0.1);
+}
+
+static zth::TimeInterval calibrationTimeout(Fsm_type& fsm) {
+	return zth::TimeInterval(1) - (fsm.t() - fsm.callbackArg()->start);
+}
+
+Fsm_type::Description desc = {
+	stateInit,
+		zth::guards::always,		stateCalibrate,
+		zth::guards::end,
+	stateCalibrate,
+		calibrationTimeout,			stateMeasure,
+		maxIterations,				stateMeasure,
+		zth::guards::always,		stateCalibrate,
+		zth::guards::end,
+	stateMeasure,
+		zth::guards::always,		stateDone,
+		zth::guards::end,
+	stateDone,
+		zth::guards::end,
 };
 
-typedef FsmDescriptionPair FsmDescription[];
-
-template <typename Context>
-class Fsm {
-public:
-	Fsm(Context& context)
-		: context(context)
-	{}
-
-	void build(FsmDescription const description, size_t size) {
-		FsmDescriptionPair const* p = description;
-		while((uintptr_t)p < (uintptr_t)description + size) {
-			printf("%p State %d:\n", p, p->state);
-			for(; p->guard.f(); ++p)
-				printf("Transition: %p -> %d\n", p->guard.f(), p[1].state);
-			++p;
-		}
+static std::string preciseTime(double s) {
+	std::string res;
+	if(s >= 0)
+		res = " ";
+	res += zth::format("%.12f", s);
+	std::string::iterator it = res.end();
+	size_t cnt = 0;
+	while(true) {
+		if(it == res.begin())
+			break;
+		if(*(it - 1) == '.')
+			break;
+		if(cnt > 0 && cnt % 3 == 0)
+			it = res.insert(it, '\'');
+		cnt++;
+		--it;
 	}
+	res += " s";
+	return res;
+}
 
-	int state() const { return m_state; }
+static void cb(Fsm_type& fsm, TestData* testData) {
+	if(!fsm.entry())
+		return;
 
-	int eval() {
-	/*
-		Transition* t = transitions[state()];
-		while(++t)
-			if(t->guard())
-				setState(t->state);
-	*/
-		return 0;
+	zth_assert(testData);
+
+	switch(fsm.state()) {
+	case stateInit:
+		testData->calibration = 9; // Do at least 10 runs.
+		testData->start = fsm.t();
+		break;
+	case stateCalibrate:
+		testData->test();
+		testData->calibration++;
+		break;
+	case stateMeasure:
+		for(size_t i = testData->calibration; i > 0; i--)
+			testData->test();
+		testData->duration = fsm.dt();
+		break;
+	case stateDone:
+		testData->singleResult += testData->duration.s() / testData->calibration;
+		printf("%-30s: %s (%8zu iterations)\n",
+			testData->description,
+			preciseTime(testData->singleResult).c_str(),
+			testData->calibration);
+		break;
 	}
+}
 
-	Context& context;
-private:
-	int m_state;
-};
+static Fsm_type::Compiler compiler(desc);
 
-class Measurement {
-public:
-	Measurement()
-		: m_fsm(*this)
-		, m_iterations(1)
-	{
-		fsmInit();
-	}
+double runTest(char const* name, void(*test)(), bool calibrationRun = false) {
+	zth::yield();
 
-	bool test() {
-		while(true)
-			switch(m_fsm.eval()) {
-			case sTestStart:
-				m_iteration = m_iterations;
-				m_start = zth::Timestamp::now();
-				return true;
-			case sTest:
-				m_iteration--;
-				return true;
-			case sTestEnd:
-				m_duration = zth::Timestamp::now() - m_start;
-				break;
-			default:
-				return false;
-			}
-	}
+	static double calibration = 0;
+	if(calibrationRun)
+		calibration = 0;
 
-	uint64_t iterations() const { return m_iterations; }
-	uint64_t iteration() const { return m_iteration; }
-	zth::TimeInterval const& duration() const { return m_duration; }
-	double t() const { return m_fsm.state() == sTestDone ? duration().s() / (double)iterations() : -1; }
+	TestData testData = { name, test, calibration };
+	Fsm_type(compiler, &cb, &testData).run();
 
-protected:
-	typedef Fsm<Measurement&> Fsm_t;
-	static bool iterationsDone(Fsm_t& fsm) { return fsm.context.iteration() == 0; }
-	static bool reachedDurationThreshold(Fsm_t& fsm) { return fsm.context.duration().s() > MEASURE_TARGET_TIME_s; }
-	enum States { sTestStart = 100, sTest, sTestEnd, sTestDone };
+	if(calibrationRun)
+		calibration = -testData.singleResult;
 
-	void fsmInit() {
-		FsmDescription transitions = {
-			sTestStart,
-				always, sTest,
-				NULL,
-			sTest,
-				iterationsDone, sTestEnd,
-				NULL,
-			sTestEnd,
-				reachedDurationThreshold, sTestDone,
-				always, sTestStart,
-				NULL,
-			sTestDone,
-				NULL,
-		};
-
-		m_fsm.build(transitions, sizeof(transitions));
-	}
-
-private:
-	Fsm_t m_fsm;
-	uint64_t m_iterations;
-	uint64_t m_iteration;
-	zth::Timestamp m_start;
-	zth::TimeInterval m_duration;
-};
-
-void measure_timestamp() {
-
-	Measurement m;
-	while(m.test())
-		zth::Timestamp::now();
-
-	printf("Timestamp(): %g us over %" PRIu64 " iterations\n", m.t() * 1e6, m.iterations());
+	return testData.singleResult - calibration;
 }
 
 void main_fiber(int argc, char** argv) {
-	measure_timestamp();
+	runTest("calibration", &testEmpty, true);
+	runTest("empty test", &testEmpty);
+	runTest("Timestamp::now()", &testNow);
+
+	testContextInit();
+	runTest("2 context_switch()s", &testContext);
+	testContextCleanup();
+	
+	runTest("single-fiber yield()", &testYield1);
+	runTest("single-fiber yield() always", &testYield1Always);
+	testYieldInit();
+	runTest("yield() to fiber and back", &testYield);
+	testYieldCleanup();
+
+	runTest("create and cleanup fiber", &testFiberCreate);
 }
 
