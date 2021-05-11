8deddbcbe4054785fa3cddef6b22694c0a558041
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-05-11 21:15:19 +0200

    typo

--- include/zth_config.h
+++ include/zth_config.h
@@ -1,17 +1,17 @@
 /*
  * Zth (libzth), a cooperative userspace multitasking library.
  * Copyright (C) 2019-2021  Jochem Rutgers
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -28,7 +28,7 @@
 #ifdef __cplusplus
 namespace zth {
 	/*!
-	 * \brief The configration of Zth.
+	 * \brief The configuration of Zth.
 	 * \ingroup zth_api_cpp_config
 	 */
 	struct Config : public DefaultConfig {
