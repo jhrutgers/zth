b7bd370bf29b8ebe6fd109a0a8016099ccfbb0c4
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-12-02 10:59:44 +0100

    fixing handling cmd parameters

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -8,20 +8,13 @@ if not exist build mkdir build
 if errorlevel 1 goto error
 
 set build_type=Release
-set cmake_args=
 
 if "%~1" == "" goto build
 set build_type=%~1
-shift
-:args
-if "%~1" == "" goto build
-set cmake_args=%cmake_args% "%~1"
-shift
-goto args
 
 :build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=%build_type% "-GMinGW Makefiles" %cmake_args% ..
+for /f "tokens=1*" %%x in ("%*") do cmake -DCMAKE_BUILD_TYPE=%build_type% "-GMinGW Makefiles" %%y ..
 if errorlevel 1 goto error_popd
 cmake --build . -- -j%NUMBER_OF_PROCESSORS%
 if errorlevel 1 goto error_popd
