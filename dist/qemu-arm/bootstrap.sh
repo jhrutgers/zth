181aee4626b69ddca31861e21f60c4ae34a371dd
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-12-03 11:16:41 +0100

    working on ARM example

--- dist/qemu-arm/bootstrap.sh
+++ dist/qemu-arm/bootstrap.sh
@@ -9,7 +9,7 @@ function gotErr {
 
 trap gotErr ERR
 
-sudo apt install -y build-essential gdb-multiarch
+sudo apt install -y build-essential gdb-multiarch qemu-system-arm
 which arm-none-eabi-g++ > /dev/null || sudo apt install -y gcc-arm-none-eabi
 which cmake > /dev/null || sudo apt install -y cmake
 which doxygen > /dev/null || sudo apt install -y doxygen
